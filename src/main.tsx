8eadbcd9bb0e7414c61fec1ac8eded38df4651e8
Bismark <agyarebismark6@gmail.com>
2025-03-09 15:54:23 +0000

    Configure CopilotKit popup with custom instructions and API key

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { CopilotKit } from "@copilotkit/react-core";
+import { CopilotPopup, CopilotSidebar } from "@copilotkit/react-ui";
+import "@copilotkit/react-ui/styles.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <CopilotKit publicApiKey={import.meta.env.VITE_COPILOT_API_KEY}>
+      <CopilotPopup
+        labels={{
+          title: "Sidebar Assistant",
+          initial: "How can I help you today?",
+        }}
+        instructions="You are assisting the user as best as you can. Answer in the best way possible given the data you have. Answer in the language of the user and in a way that is easy to understand. Let your responses be only limited to the scope of this website and responses related to well-being. No other information aside the content of this website should be shared"
+      />
+      <App />
+    </CopilotKit>
   </StrictMode>
 );
+
