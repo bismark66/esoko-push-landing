fad1b2bd33b341ea098529368034b08a39948d92
Bismark <agyarebismark6@gmail.com>
2025-03-11 16:07:27 +0000

    Update CopilotKit sidebar instructions with fallback contact support message

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")!).render(
           title: "Sidebar Assistant",
           initial: "How can I help you today?",
         }}
-        instructions="You are assisting the user as best as you can. Answer in the best way possible given the data you have. Answer in the language of the user and in a way that is easy to understand. Let your responses be only limited to the scope of this website and responses related to well-being. No other information aside the content of this website should be shared"
+        instructions="You are assisting the user as best as you can. Answer in the best way possible given the data you have. Answer in the language of the user and in a way that is easy to understand. Let your responses be only limited to the scope of this website and responses related to well-being. No other information aside the content of this website should be shared. If you have no contextual information, just say, contact support at +233246347907"
       />
       <App />
     </CopilotKit>
